a89545b6fbe3945079ef8114ffaf1d80a8912a38
Here <tgb3308@naver.com>
2024-04-15 17:28:22 +0900

    Arang
    last

---
+++ src/main/java/com/medici/arang/shop/command/ShopItemCommand.java
@@ -0,0 +1,28 @@
+package com.medici.arang.shop.command;
+
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class ShopItemCommand {
+	
+	private String cateCodeRef;
+	private String cateCode;
+	private String itemName;
+	private double itemPrice;
+	private int itemNum;
+	private	int itemStock;
+	private String itemInfo;
+	private String itemImg;
+	private Date regDate;
+	private Date nada;
+	
+	public ShopItemCommand() {
+		super();
+	}
+}
